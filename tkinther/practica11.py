70caece68eb884beaf92c2dfb27d30f6a5abb77e
lOkY <102552151+IvanIsay@users.noreply.github.com>
2023-03-01 16:21:13 -0600

    Tkinter: funciones con botones

--- tkinther/practica11.py
+++ tkinther/practica11.py
@@ -1,8 +1,22 @@
 from tkinter import Tk,Button,Frame,messagebox
 
-#5. declaramos funciones
+#5. declaramos funcion para mensaje
 def mostrarMensajes():
     messagebox.showinfo("Aviso:","Presionaste el boton azul")
+    messagebox.showerror("Error:","Todo fallo con exito")
+    print(messagebox.askokcancel("Pregunta","¿Ella o el  jugo con tu corazon?"))
+ 
+
+#6. FUncion para agregar botones
+def agregarBoton():
+    botonVerde.config(text="+",bg="green",fg="white")
+    botonNuevo= Button(seccion3,text="Nuevo")
+    botonNuevo.pack()
+
+
+
+
+ 
     
 
 
@@ -32,10 +46,12 @@ botonNegro.grid(row=0 , column=0)
 botonAmarillo =Button(seccion2,text="Boton Amarillo", bg="#ffff4d")
 botonAmarillo.grid(row=1 , column=1)
 
-botonVerde=Button(seccion3,text="Boton Verde", bg="#99e699")
+botonVerde=Button(seccion3,text="Boton Verde", bg="#99e699", command=agregarBoton)
 botonVerde.configure(height=2,width=10)
 botonVerde.pack()
 
+
+#4. Poscionamiento de elemntos 
 
 
 #llamamos al Main
