73a5a1b6116d91291de9aab74dc534d7c133d917
lOkY <102552151+IvanIsay@users.noreply.github.com>
2023-03-22 17:15:58 -0600

    Se preparo la vista y BD para el formulario registro

---
+++ tkinterSqlite/UsuariosCRUD.py
@@ -0,0 +1,43 @@
+
+from tkinter import * 
+from tkinter import ttk
+import tkinter as tk
+
+Ventana= Tk()
+Ventana.title("CRUD Usuarios")
+Ventana.geometry("500x300")
+
+panel= ttk.Notebook(Ventana)
+panel.pack(fill="both",expand= "yes")
+
+pestana1= ttk.Frame(panel)
+pestana2= ttk.Frame(panel)
+pestana3= ttk.Frame(panel)
+pestana4= ttk.Frame(panel)
+
+#Pestana1: Formulario Usuarios
+titulo= Label(pestana1,text="Registro Usuarios",fg="Blue",font=("Modern",18) ).pack()
+
+varNom= tk.StringVar()
+lblNom= Label(pestana1, text="Nombre: ").pack()
+txtNom= Entry(pestana1,textvariable=varNom).pack()
+
+varCor= tk.StringVar()
+lblCor= Label(pestana1, text="Correo: ").pack()
+txtCor= Entry(pestana1,textvariable=varCor).pack()
+
+varCon= tk.StringVar()
+lblCon= Label(pestana1, text="Contraseña: ").pack()
+txtCon= Entry(pestana1,textvariable=varCon).pack()
+
+btnGuardar= Button(pestana1,text="Guardar usuario").pack()
+
+
+
+
+panel.add(pestana1,text="Formulario de usuarios")
+panel.add(pestana2,text="Buscar Usuario")
+panel.add(pestana3,text="Consultar Usuarios")
+panel.add(pestana4,text="Actualizar Usuario")
+
+Ventana.mainloop()
